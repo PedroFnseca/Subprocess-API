27ad64dd4f0798b2517654db08b4dc38847f2f70
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-19 02:46:52 -0300

    Configuração da index

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import routes from './routes'
+import { config } from 'dotenv'
+
+config()
+
+const api = express()
+
+const PORT = process.env.PORT || 3333
+
+api.use(express.json())
+api.use('/', routes)
+
+api.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
