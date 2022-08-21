67305f08f6299e95648cac4b556c65feb8a747a3
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-21 19:46:26 -0300

    Adicionando mensagem

--- src/index.js
+++ src/index.js
@@ -13,12 +13,15 @@ setInterval(async () => {
     // coleta a data e hora atual
     const now = new Date()
     
-    console.log(`Iniciando verficações: ${now.toLocaleString('pt-BR')}`)
+    console.log(`Iniciando verficações: ${now.toLocaleString('pt-BR')}\n`)
     
     // verifica se a hora atual é a hora de verificar os empréstimos
     if(now.getHours() === parseInt(process.env.HOUR_TO_VERIFY)){
-        console.log('Iniciando verificações de empréstimos')
+        console.log('Iniciando verificações de empréstimos\n')
         await verifyLendingBook()
+        console.log('Verificações de empréstimos concluidas')
+    } else {
+        console.log(`Os emprestimos serão verificados às ${process.env.HOUR_TO_VERIFY}:00\n`)
     }
 
     console.log(`Verificações concluidas: ${now.toLocaleString('pt-BR')}`)    
