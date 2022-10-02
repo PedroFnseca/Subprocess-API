7fd2063ae5631521e5cb1c3ff28809ccb91b4e17
Pedro Fonseca <97262778+PedroFnseca@users.noreply.github.com>
2022-10-02 10:44:50 -0300

    Update lending.js

--- src/database/lending.js
+++ src/database/lending.js
@@ -3,7 +3,7 @@ import db from './config.js'
 // Verifica os emprestimos que estão proximos ao vencimento de 1 dia
 async function checkLendingBook1() {
     const conn = await db.connect()
-    const [rows] = await conn.query('SELECT * FROM VW_lending_CloseToDate_1')
+    const [rows] = await conn.query('SELECT * FROM VW_lending_CloseToDate_4')
     conn.end()
     return rows
 }
